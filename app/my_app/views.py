aacd0c1c0a2c488bd7e6d47f4e2a491413b75662
Andre <aldutoit@hotmail.com>
2022-09-12 12:51:22 +0200

    Cleaning up whitespaces

--- app/my_app/views.py
+++ app/my_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-     return HttpResponse( "Howzit my Broer!!!" )
+     return HttpResponse("Howzit my Broer!!!")
